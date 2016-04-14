8dd9d8bce2858c41fcff9c7837589f1f5188ec6a
Brent Freeman <brfree91@hotmail.com>
2016-04-14 18:20:23 -0500

    Log in and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+   log_in @user
      flash[:success] = "Sign-Up Was successful, Welcome"
       redirect_to @user
     else
